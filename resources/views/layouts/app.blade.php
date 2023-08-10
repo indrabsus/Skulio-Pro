307cf23986ff3ea755a3eb289a97abfad1198fc4
Indra Batara <indrabsus@Indras-MacBook-Pro.local>
2023-08-10 21:58:10 +0700

    logo ganti fav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="{{asset('adminv/assets/img/rfid.png')}}">
+  <link rel="icon" type="image/x-icon" href="{{asset('adminv/assets/img/logo.png')}}">
   @livewireStyles
   <meta name="csrf-token" content="{{ csrf_token() }}">
 <script src="{{asset('js/jquery.js')}}"></script>
